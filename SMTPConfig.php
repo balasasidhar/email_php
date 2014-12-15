0acd3bd358e974d2e3935ba87c362694e635fde7
Bala Sasidhar Kola <sasidhar.678@gmail.com>
2014-12-15 21:45:18 +0530

    Create SMTPConfig.php

---
+++ SMTPConfig.php
@@ -0,0 +1,11 @@
+<?php
+	
+	//Server Configuration goes here... 
+
+	$SmtpServer    =   "mail.mydomain.com";            // smtp server
+	$SmtpPort      =   "25";                           // port number
+	$SmtpUser      =   "username@mydomain.com";        // smtp username
+	$SmtpPass      =   "password";                     // smtp password
+	$from          =   "email@mydomain.com";           // email from address
+
+?>
