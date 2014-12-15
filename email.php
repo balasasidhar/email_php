fc33abb54bf82239724df6aea5c637c7c5c27b7b
Bala Sasidhar Kola <sasidhar.678@gmail.com>
2014-12-15 21:48:39 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,10 +1,11 @@
 <?php 
-
+	// invoke this to send an Email
+	
 	require ('SMTPClass.php');
 
-	$to        =   "emailId@domain.com";        // To address
+	$to        =   "emailId@domain.com";       // To address
 	$subject   =   "Email Subject";            // Email Subject
-	$body      =   "Mail Body";                   // Email Nody
+	$body      =   "Mail Body";                // Email Nody
 	
 	$SMTPMail = new SMTPClient($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
 	$SMTPMail->SendMail();
